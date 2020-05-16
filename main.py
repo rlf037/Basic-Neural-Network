6738f8904246d77b2ecc11c6058446b05d5b04a7
Richard Friedman <rlf613@me.com>
2020-05-16 23:32:27 +1000

    added dropout v0.55

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/Users/rlf/anaconda3/bin/python
-
 import numpy as np
 from neuralnetwork import NNet
 import matplotlib.pyplot as plt
@@ -8,20 +7,25 @@ import matplotlib.pyplot as plt
 with np.load('datasets/mnist.npz') as data:
     X, Y = data['X'], data['Y']
 
-# X = X[:7000]
-# Y = Y[:7000]
+# === HOUSE PRICES ===
+# with np.load('datasets/houseprices.npz') as data:
+#     X_train, Y_train, X_test = data['X_train'], data['Y_train'], data['X_test']
 
+X = X[:700]
+Y = Y[:700]
+
+# === PRE-PROCESSING ===
+Y, code = NNet.Encode(Y)
+X = NNet.Normalize(X)
+X_train, X_test, Y_train, Y_test = NNet.Split(X, Y)
 # === NEURAL NETWORK ===
-model = NNet(verbose=True)
-Y, code = NNet.encode(Y)
-X = NNet.normalize(X)
-X_train, X_test, Y_train, Y_test = NNet.split(X, Y)
+model = NNet(verbose=False)
 model.input(X_train.shape)
-model.hidden(neurons=128)
-model.hidden(neurons=64)
-model.hidden(neurons=64)
+model.hidden(neurons=64, activation='leaky_relu', dropout=True)
+model.hidden(neurons=128, activation='sigmoid', dropout=False)
+model.hidden(neurons=64, activation='leaky_relu', dropout=False)
 model.output(Y_train.shape, activation='softmax')
-model.compile()
+model.compile(loss='cce')
 model.train(X_train, Y_train)
 model.evaluate(X_test, Y_test)
 
@@ -41,7 +45,7 @@ model.evaluate(X_test, Y_test)
 # scce for integers
 
 
-#implement dropout
+# implement dropout
 
-#convert from one-hot encoding back to category integers and then decode back to original class
+# convert from one-hot encoding back to category integers and then decode back to original class
 # with self.code dictionary that stored each class as a value to the integer key
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,38 +1,42 @@
 import numpy as np
-import warnings, dill, math, itertools
-# import tensorflow as tf
+import dill
+import tensorflow as tf
 
 class NNet:
 
     def __init__(self, verbose=False):
         if not isinstance(verbose, bool):
             raise TypeError("verbose must be True or False")
+        self.cce = tf.keras.losses.CategoricalCrossentropy()
         self.verbose = verbose
-        self.layers = 0
         self.params = 0
-        self.activations = []
         self.weights = []
         self.biases = []
+        self.activations = []
+        self.dropouts = []
         self.inputExists = False
         self.outputExists = False
+        self.dropout_num = .2
+        self.glorot = 6
+        self.he = 2
         self.activators = ['relu', 'tanh', 'sigmoid', 'softmax', 'leaky_relu']
         self.optimizers = ["adam", "sgd", "rmsprop", "adadelta"]
         self.loss_functions = ['mae', 'mse', 'cce', 'scce', 'bce']
-        self.scorers = ['accuracy']
+        self.scorers = ['accuracy', 'loss']
         if self.verbose:
             print("* Neural Network Initialised *\n")
 
     @staticmethod
-    def encode(target):
-        if str(target.dtype)[:2] != '<U':
-            raise TypeError(f"Cannot encode: {target.dtype} must be string/unicode format")
-        output_size = len(np.unique(target))
+    def Encode(labels):
+        if str(labels.dtype)[:2] != '<U':
+            raise TypeError(f"Cannot encode {labels.dtype} must be string/unicode format")
+        output_size = len(np.unique(labels))
         count = 0
         output = []
         output_dict = {}
         total = set()
         code = {}
-        for i in target:
+        for i in labels:
             if i in output_dict:
                 output.append(output_dict[i])
             else:
@@ -44,19 +48,19 @@ class NNet:
         return np.eye(output_size)[output], code
 
     @staticmethod
-    def normalize(data):
-            xmax, xmin = np.amax(data), np.amin(data)
-            normalize = lambda x: (x - xmin) / (xmax - xmin)
-            return normalize(data)
+    def Normalize(data):
+        xmax, xmin = np.amax(data), np.amin(data)
+        normalize = lambda x: (x - xmin) / (xmax - xmin)
+        return normalize(data)
 
     @staticmethod
-    def standardize(data):
-            xmean, xstd = np.mean(data), np.std(data)
-            standardize = lambda x: (x - xmean) / xstd
-            return standardize(data)
+    def Standardize(data):
+        xmean, xstd = np.mean(data), np.std(data)
+        standardize = lambda x: (x - xmean) / xstd
+        return standardize(data)
 
     @staticmethod
-    def split(data, target, test_split=1/5, shuffle=True, seed=None):
+    def Split(data, target, test_split=1/5, shuffle=True, seed=None):
         if data is None:
             raise ValueError("X is None")
         if target is None:
@@ -82,7 +86,6 @@ class NNet:
                 data, target = data[rp], target[rp]
             test_size = int(samples * test_split)
             train_size = samples - test_size
-            assert (test_size + train_size) == samples
             return data[:train_size], data[-test_size:], target[:train_size], target[-test_size:]
         else:
             raise ValueError(f"Test split {test_split} must be a value between (excluding) 0 and 1")
@@ -91,7 +94,7 @@ class NNet:
         if not isinstance(input_size, tuple):
             raise TypeError("Input size must be a tuple")
         if not input_size:
-            raise ValueError("Input size is none")
+            raise ValueError("Input size is None")
         if len(input_size) > 2:
             self.flatten = True
             if not (input_size[1] == input_size[2]):
@@ -104,12 +107,13 @@ class NNet:
             self.input_size = input_size[1]
 
         self.inputExists = True
+        self.sample_size = input_size[0]
 
         if self.verbose:
             if self.flatten:
                 print(f"Flattened:\t({int(self.input_size**0.5)}, {int(self.input_size**0.5)}) -> {self.input_size}")
 
-    def hidden(self, neurons=128, activation="relu", dropout=False):
+    def hidden(self, neurons=50, activation="relu", dropout=False):
         if not self.inputExists:
             raise NotImplementedError("No input layer implemented yet - try input() first")
         if neurons<1 or neurons>1024 or not isinstance(neurons, int):
@@ -120,11 +124,11 @@ class NNet:
             self.activations.append(activation)
         if not isinstance(dropout, bool):
             raise TypeError("Dropout must be True or False")
-
-        self.layers += 1
+        else:
+            self.dropouts.append(dropout)
 
         # get amount of input neurons needed for this layer from the previous layer's output size
-        if self.layers == 1:
+        if len(self.weights) == 0:
             # FIRST LAYER ONLY
             input_neurons = self.input_size
             if self.verbose:
@@ -134,13 +138,16 @@ class NNet:
             # ALL OTHER LAYERS
             input_neurons = self.previous_output_size
 
+        if activation == 'relu' or activation == 'leaky_relu':
+            init = self.he
+        else:
+            init = self.glorot
+
         # parameter counter
         self.params += (input_neurons * neurons) + neurons
-        # INITIALIZE WEIGHTS
-        self.weights.append(np.random.randn(input_neurons, neurons) * np.sqrt(1/input_neurons))
-        # INITIALIZE BIASES
+        # INITIALIZE WEIGHTS & BIASES
+        self.weights.append(np.random.randn(input_neurons, neurons) * np.sqrt(init/input_neurons))
         self.biases.append(np.random.randn(1, neurons))
-
         # set this layer's output neurons for the next layer
         self.previous_output_size = neurons
 
@@ -151,8 +158,14 @@ class NNet:
                 print(f"\t\t|\t\t\nHidden [{neurons}] ({activation})")
 
     def output(self, output_size, activation=None):
-        if self.layers == 0:
+        if len(self.weights) == 0:
             raise NotImplementedError("No hidden layer(s) implemented yet - try hidden() first")
+        if not isinstance(output_size, tuple):
+            raise TypeError("Output size must be a tuple")
+        if not output_size:
+            raise ValueError("Output size is None")
+        if output_size[0] != self.sample_size:
+            raise ValueError(f"Input and Output have unequal sample sizes. ({self.sample_size} vs. {output_size[0]})")
         if activation not in self.activators:
             raise ValueError(f"{activation} is not a valid activation function. Options: {self.activators}")
         else:
@@ -166,9 +179,13 @@ class NNet:
 
         self.outputExists = True
 
-        # parameter counter 
+        if activation == 'relu' or activation == 'leaky_relu':
+            init = self.he
+        else:
+            init = self.glorot
+
         self.params += (self.previous_output_size * self.output_size) + self.output_size
-        self.weights.append(np.random.randn(self.previous_output_size, self.output_size) * np.sqrt(1/self.previous_output_size))
+        self.weights.append(np.random.randn(self.previous_output_size, self.output_size) * np.sqrt(init/self.previous_output_size))
         self.biases.append(np.random.randn(1, self.output_size))
         
         if self.verbose:
@@ -179,7 +196,6 @@ class NNet:
     def compile(
         self, valid_split=1/10, optimizer="adam", loss="cce",
         scorer="accuracy", learn_rate=1e-3, batch_size=64, epochs=15):
-
         if not self.outputExists:
             raise NotImplementedError("No output layer implemented yet - try output() first")
         if optimizer not in self.optimizers:
@@ -194,32 +210,26 @@ class NNet:
             raise ValueError(f"{scorer} is not a valid scorer. Options: {self.scorers}")
         else:
             self.scorer = scorer
-
         if batch_size>0 and isinstance(batch_size, int):
             self.batch_size = batch_size
         else:
             raise ValueError(f"Batch size {batch_size} must be an integer greater than 1")
-        if batch_size > 128:
-            warnings.warn("Batch sizes greater than 128 are not recommended")
         if epochs > 0 and epochs < 1000 and isinstance(epochs, int):
             self.epochs = epochs
         else:
             raise ValueError(f"({epochs}) Number of epochs must be an integer between 1 and 999")
-
         self.learn_rate = learn_rate
-
         if valid_split>0 and valid_split <1:
             self.valid_split = valid_split
         else:
             raise ValueError(f"({valid_split}) Validation split must be a value between 0 and 1")
-
         if self.verbose:
-            keys = locals()
-            del keys['self']
-            print(keys)
+            settings = locals()
+            del settings['self']
+            print(settings)
 
     @staticmethod
-    def Activation(act, x, d=False):
+    def Activate(act, x, d=False):
         if act == 'sigmoid':
             if d:
                return (1.0/(1.0 + np.exp(-x))) * (1-(1.0/(1.0 + np.exp(-x))))
@@ -257,7 +267,10 @@ class NNet:
         if loss == 'mae': #L1
             return np.sum(np.absolute(y_pred - y_true))
         if loss == 'mse': #L2
-            return np.sum((y_pred - y_true)**2) / y_true.size
+            losses = []
+            for pred, true in zip(y_pred, y_true):
+                losses.append(np.sum((pred - true)**2) / y_true.size)
+            return np.mean(losses)
 
     def train(self, data, target):
         if data is None:
@@ -279,82 +292,88 @@ class NNet:
         # flatten data if applicable before being trained
         if self.flatten:
             data = data.reshape(data.shape[0], -1)
-
         # split into training and validation data
         self.valid_size = int(data.shape[0] * self.valid_split)
         self.train_size = data.shape[0] - self.valid_size
-        assert (self.train_size + self.valid_size) == data.shape[0]
         X_train, self.X_valid = (data[:self.train_size], data[-self.valid_size:])
         Y_train, self.Y_valid = (target[:self.train_size], target[-self.valid_size:])
-
-        # reset batch size if greater than training samples
+        # reset batch size if greater than training size
         if self.batch_size >= self.train_size:
             self.batch_size = self.train_size
-
-        # if self.verbose:
+# ====================================================================================
         print(f'\nTraining on {self.train_size:,} samples...')
 
-        #loop for each epoch
+        # EPOCH ITERATION
         for i in range(self.epochs):
-            # batch stuff
-            sbatch = 0
-            ebatch = self.batch_size
-            batch_finished = False
+            
+            # batch info
+            sample_start = 0
+            sample_end = self.batch_size
+            current_batch_finished = False
             is_last_run = False
-            batches = int(math.ceil(self.train_size/self.batch_size))
-            count = 1
 
-            # loop through batches
-            while not batch_finished:
-                # print the epoch, batch and sample info
-                #set batches
-                xtrain, ytrain = X_train[sbatch:ebatch], Y_train[sbatch:ebatch]
+            # DROPOUT
+            if not self.dropouts:
+                pass
+            else:
+                self.dropout(self.dropout_num)
 
-                # get output of a batch forward pass
-                output = self.forward(xtrain)
+            # BATCH ITERATION
+            while not current_batch_finished:
+                #set the batch sample
+                X, Y = X_train[sample_start:sample_end], Y_train[sample_start:sample_end]
 
-                # calculate loss from the output of the forward pass to ytrain
-                loss = self.Loss(self.loss, output, ytrain)
-                # cce = tf.keras.losses.CategoricalCrossentropy()
-                # loss2 = cce(ytrain, output)
-                # print(f"({loss2}")
-                # gradient descent (backwards pass here)
-                print(f"Epoch {i+1}/{self.epochs}\tSamples {ebatch}/{self.train_size}\tTrain Loss {loss:4f}", end='\r')
-                self.backProp(loss, output, ytrain)
+                # get the output of a feed forward
+                output = self.forward(X)
 
-                # move to the next batch
-                sbatch = ebatch
-                # if the next batch will go equal or after the training size
-                if ebatch + self.batch_size >= self.train_size:
-                    ebatch = self.train_size
-                    # end the batch loop
+                # calculate loss from the output of the feed forward and the actual Y
+                ####loss = self.Loss(self.loss, Y, output)
+                loss = self.cce(Y, output)
+
+                # ========== GRADIENT DESCENT [BACKPROPAGATION] ==========
+                self.backprop(loss)
+
+                print(f"Epoch {i+1}/{self.epochs}\tSamples {sample_end}/{self.train_size}\tTrain Loss {loss:4f}",
+                end='\r')
+                
+                # setup next batch
+                sample_start = sample_end
+                # if the next batch will go equal or beyond the training sample size
+                # set the end of the batch to the training size
+                if sample_end + self.batch_size >= self.train_size:
+                    sample_end = self.train_size
+                    # if it was it's last run, it's now complete
                     if is_last_run:
-                        batch_finished = True
-                    # set the last batch run
+                        current_batch_finished = True
+                    # and set the batch loop to it's last run
                     is_last_run = True
                 else:
-                    # increase the end batch num by batch size
-                    ebatch += self.batch_size
+                    # increase the end of the batch samples by a batch size
+                    sample_end += self.batch_size
 
-                count += 1
-
-            # validate the newly optimized weights and biases
+            # validate the newly optimized weights and biases with new data
             valid_loss, valid_acc = self.validate()
 
-            #print loss and accuracy after each epoch
+            #print loss and accuracy after each epoch with valid data
             print(f"\nVal Loss: {loss:.4f} | Val Accuracy: {valid_acc:.3%}")
             # self.output = np.concatenate(output_list)
 
-    def forward(self, X):
+    def dropout(self, num):
+        for index, drop in enumerate(self.dropouts):
+            if drop:
+                drops = np.random.binomial(1, 1-num, self.weights[index].shape)
+                self.weights[index] *= drops
 
+    def forward(self, X):
         for weight, bias, activation in zip(self.weights, self.biases, self.activations):
             X = np.dot(X, weight) + bias
-            X = self.Activation(activation, X)
+            X = self.Activate(activation, X)
         return X
 
-    def backProp(self, loss, output, ytrain):
+    def backprop(self, loss):
+        momentum = 0.9
 
-        pass
+        #error_grad = 
         # cost = np.mean(ytrain-output)
         # print(cost)
         # delta = self.Activation(self.activations[-1], loss, d=True) * cost
